c54150ec9b1cc47700994882121d10b46cb9d723
Jon <jmunoa91@gmail.com>
2023-05-04 07:41:47 +0200

    Create test.pm

---
+++ test.pm
@@ -0,0 +1,49 @@
+package MyModule;
+
+sub hello {
+    print "Hola, mundo!\n";
+}
+
+1;
+package MyModule;
+
+sub hello {
+    print "Hola, mundo!\n";
+}
+
+1;
+package MyModule;
+
+sub hello {
+    print "Hola, mundo!\n";
+}
+
+1;
+package MyModule;
+
+sub hello {
+    print "Hola, mundo!\n";
+}
+
+1;
+package MyModule;
+
+sub hello {
+    print "Hola, mundo!\n";
+}
+
+1;
+package MyModule;
+
+sub hello {
+    print "Hola, mundo!\n";
+}
+
+1;
+package MyModule;
+
+sub hello {
+    print "Hola, mundo!\n";
+}
+
+1;
