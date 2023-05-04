7a00ffb44ff06acfcf36263ac5ec3cde6adad3d7
Jon <jmunoa91@gmail.com>
2023-05-04 07:39:16 +0200

    Create test.vue

---
+++ test.vue
@@ -0,0 +1,19 @@
+<!-- Definición de un componente Vue -->
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      title: 'Hola Mundo!',
+      message: '¡Bienvenidos a mi aplicación Vue!'
+    }
+  }
+}
+</script>
