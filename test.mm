c1ef0bd203b2ae7e28eb8f0527a59fc05e0e6cc9
Jon <jmunoa91@gmail.com>
2023-05-04 09:50:06 +0200

    Create test.mm

---
+++ test.mm
@@ -0,0 +1,64 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C++!");
+    }
+    return 0;
+}
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C++!");
+    }
+    return 0;
+}
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C++!");
+    }
+    return 0;
+}
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C++!");
+    }
+    return 0;
+}
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C++!");
+    }
+    return 0;
+}
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C++!");
+    }
+    return 0;
+}
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C++!");
+    }
+    return 0;
+}
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, Objective-C++!");
+    }
+    return 0;
+}
