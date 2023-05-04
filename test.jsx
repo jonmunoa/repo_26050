8405bde1b2ab1a1ac33e6e9c931b64fc2a244dcf
Jon <jmunoa91@gmail.com>
2023-05-04 09:49:38 +0200

    Create test.jsx

---
+++ test.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, JSX!</h1>,
+  document.getElementById('root')
+);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, JSX!</h1>,
+  document.getElementById('root')
+);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, JSX!</h1>,
+  document.getElementById('root')
+);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, JSX!</h1>,
+  document.getElementById('root')
+);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, JSX!</h1>,
+  document.getElementById('root')
+);
