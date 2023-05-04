18c3765edb408d15e5eb7d1e7e552ca2b7fd7eb8
Jon <jmunoa91@gmail.com>
2023-05-04 09:49:18 +0200

    Create test.groovy

---
+++ test.groovy
@@ -0,0 +1,25 @@
+class HelloWorld {
+    static void main(String[] args) {
+        println("Hello, Groovy!")
+    }
+}
+class HelloWorld {
+    static void main(String[] args) {
+        println("Hello, Groovy!")
+    }
+}
+class HelloWorld {
+    static void main(String[] args) {
+        println("Hello, Groovy!")
+    }
+}
+class HelloWorld {
+    static void main(String[] args) {
+        println("Hello, Groovy!")
+    }
+}
+class HelloWorld {
+    static void main(String[] args) {
+        println("Hello, Groovy!")
+    }
+}
