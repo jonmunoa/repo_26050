ab5536602314db818e6fa6bd8295e6762b60cfd8
Jon <jmunoa91@gmail.com>
2023-05-04 07:42:16 +0200

    Create test.dpr

---
+++ test.dpr
@@ -0,0 +1,42 @@
+program MiProyecto;
+
+uses
+  Forms,
+  MainForm in 'MainForm.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+program MiProyecto;
+
+uses
+  Forms,
+  MainForm in 'MainForm.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+program MiProyecto;
+
+uses
+  Forms,
+  MainForm in 'MainForm.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
